c0930998624cecc6bfb222f054d0cb64244f1854
herman griffin <97112+frek818@users.noreply.github.com>
2022-09-19 01:03:33 -0400

    fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR
+cd $SCRIPT_DIR/..
 
 # generate new data
-python constituents.py
+/usr/local/bin/python scripts/constituents.py
 
 # Save changes
-git add ../data/constituents.csv 
-git add ../data/constituents_symbols.txt
+git add data/constituents.csv 
+git add data/constituents_symbols.txt
 git commit -m "[data][skip ci] automatic update" || exit 0
 git push origin
 
